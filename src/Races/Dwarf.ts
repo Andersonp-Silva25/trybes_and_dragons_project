7191d40580322c1de73fb89916b68425e1d404a8
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-01-18 16:59:44 -0300

    :sparkles: feat: Criando as classes que herdam de Race

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Dwarft extends Race {
+  private _maxLifePoints: number;
+  private static _createdRacesInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarft._createdRacesInstances += 1;
+  }
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return this._createdRacesInstances;
+  }
+}
